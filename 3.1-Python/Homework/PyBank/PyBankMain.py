a82e51c1b0e1207daf5be777f388224b16e01200
Abla Elsergany <anelserg@ncsu.edu>
2019-02-17 23:00:55 -0500

    the txt file and csv file for the code outputs, and most of the code for the 2 assignments except for linking the date column with the profit increase and decrease in the budget code

--- 3.1-Python/Homework/PyBank/PyBankMain.py
+++ 3.1-Python/Homework/PyBank/PyBankMain.py
@@ -19,6 +19,87 @@ with open(csvpath, newline='') as csvfile:
     csv_header = next(budgetCsvReader)
     print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
-    #for row in csvreader:
-        #print(row)
+    # setting variables to 0
+    monthsTotal = 0
+    netTotalProfitLosses = 0
+    previousRowValue = 0
+    currentRowValue = 0
+    
+    # starting and empty list to add to it later the change in profit between every month
+    changeList = []
+    
+    # starting a for loop to read in the csv file
+    for row in budgetCsvReader:
+        #total number of months in the dataset
+        monthsTotal = monthsTotal + 1
+        #creating the variables for current row and previous row to make the 
+        # difference between them
+        currentRowValue = int(row[1])
+        # the difference btween the months
+        netTotalProfitLosses = netTotalProfitLosses + currentRowValue
+
+        # starting an if condition to exclude th every first row from throwing 
+        # an error when we come to subtract the current row from it
+        if previousRowValue != 0:
+            change = currentRowValue - previousRowValue
+            changeList.append(change)
+        
+        
+        # so that it starts the loop all over again
+        previousRowValue = currentRowValue
+    # calculating the average change in profits and losses
+    averageChange = sum(changeList) /len(changeList)
+    
+    # Max profits
+    greatestIncreaseInProfists = max(changeList)
+    
+    # Min profits
+    greatestDecreaseInLosses = min(changeList)
+
+    # printing out the output    
+    print("Financial Analysis")
+    print("                       ")
+    print("-----------------------------")
+    print("                       ")
+    print("Total Months: " + str(monthsTotal))
+    print("Total: " + str(netTotalProfitLosses))
+    print("Average Change: " + str(averageChange))
+    print("Greatest Increase In Profits: " + str(greatestIncreaseInProfists))
+    print("Greatest Decrease In Profits: " + str(greatestDecreaseInLosses))
+    
+### starting the code to write to a csv file
+# Specify the file to write to
+output_path = os.path.join(".", "BudgetWriter.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvWfile:
+
+    # Initialize csv.writer
+    budgetCsvWriter = csv.writer(csvWfile, delimiter=',')
+
+    # Write the first row (column headers)
+    budgetCsvWriter.writerow(['Financial Analysis'])
+
+    # Write the second row
+    budgetCsvWriter.writerow(['                    '])
+
+    # Write the third row
+    budgetCsvWriter.writerow(['-----------------------------'])
+
+    # Write the fourth row
+    budgetCsvWriter.writerow(['                    '])
+
+    # Write the fifth row
+    budgetCsvWriter.writerow(["Total Months: " + str(monthsTotal)])
+
+    # Write the sixth row
+    budgetCsvWriter.writerow(["Total: " + str(netTotalProfitLosses)])
+
+    # Write the seventh row
+    budgetCsvWriter.writerow(["Average Change: " + str(averageChange)])
+
+    # Write the eightth row
+    budgetCsvWriter.writerow(["Greatest Increase In Profits: " + str(greatestIncreaseInProfists)])
+
+    # Write the ninth row
+    budgetCsvWriter.writerow(["Greatest Decrease In Profits: " + str(greatestDecreaseInLosses)])
--- 3.1-Python/Homework/PyPoll/PyPollMain.py
+++ 3.1-Python/Homework/PyPoll/PyPollMain.py
@@ -4,6 +4,9 @@ import os
 # Module for reading CSV files
 import csv
 
+#importing operator library
+import operator
+
 csvpath = os.path.join('.', 'election_data.csv')
 
 #Reading using CSV module
@@ -13,12 +16,137 @@ with open(csvpath, newline='') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     electionCsvReader = csv.reader(csvfile, delimiter=',')
 
-    print(electionCsvReader)
+    #print(electionCsvReader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(electionCsvReader)
     print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
-    #for row in csvreader:
-        #print(row)
+    # setting variables to 0 and initiating and empty list and mepty dictionary
+    totalVotes = 0
+    candidateNameList = []
+    candVotesPercCnt = {}
+    KhanCount = 0
+    CorreyCount = 0
+    LiCount = 0
+    OTooleyCount = 0
+
+
+    # starting a for loop to read in the csv file
+    for row in electionCsvReader:
+        #total number of votes in the dataset
+        totalVotes = totalVotes + 1
+
+        # Adding candidates names to a list
+        if row[2] not in candidateNameList:
+            candidateNameList.append(row[2])
+
+        # creating an if conditional to get the count and percentage for each candidate 
+        # and adding them to a dictionary as the value list for the candidate name as the key
+        if row[2] == 'Khan':
+            KhanCount = KhanCount + 1
+            KhanPercentage = KhanCount / totalVotes * 100
+            round(KhanPercentage, 3)
+            candVotesPercCnt['Khan'] = (KhanPercentage, KhanCount)
+        elif row[2] == 'Correy':
+            CorreyCount = CorreyCount + 1
+            CorreyPercentage = CorreyCount / totalVotes * 100
+            candVotesPercCnt['Correy'] = (CorreyPercentage, CorreyCount)
+        elif row[2] == 'Li':
+            LiCount = LiCount + 1
+            LiPercentage = LiCount / totalVotes * 100
+            candVotesPercCnt['Li'] = (LiPercentage, LiCount)
+        elif row[2] == "O'Tooley":
+            OTooleyCount = OTooleyCount + 1
+            OTooleyPercentage = OTooleyCount / totalVotes * 100
+            candVotesPercCnt["O'Tooley"] = (OTooleyPercentage, OTooleyCount)
+        
+    # getting the election winner using the operator library and a max function
+    winner = max(candVotesPercCnt.items(), key=operator.itemgetter(1))
+    print(winner)
+
+
+
+    # printing out the output    
+    print("Election Results")
+    print(" ")
+    print("-----------------------------")
+    print("  ")
+    print("Total Votes: " + str(totalVotes))
+    print(" ")
+    print("-----------------------------")
+    print(" ")
+    print("Khan: " + str(candVotesPercCnt['Khan'][0]) + "% " + "(" + str(candVotesPercCnt['Khan'][1]) + ")")
+    print("Correy: " + str(candVotesPercCnt['Correy'][0]) + "% " + "(" + str(candVotesPercCnt['Correy'][1]) + ")")
+    print("Li: " + str(candVotesPercCnt['Li'][0]) + "% " + "(" + str(candVotesPercCnt['Li'][1]) + ")")
+    print("O'Tooley: " + str(candVotesPercCnt["O'Tooley"][0]) + "% " + "(" + str(candVotesPercCnt["O'Tooley"][1]) + ")")
+    print("  ")
+    print("-----------------------------")
+    print("   ")
+    print("Winner: " + winner[0]) 
+    print("  ")
+    print("-----------------------------")   
+
+### starting the code to write to a txt file
+# Specify the file to write to
+output_path = os.path.join(".", "PollWriter.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as fw:
+
+    # Initialize csv.writer
+    # pollCsvWriter = csv.writer(csvWfile, delimiter=',')
+
+    # Write the first row (column headers)
+    fw.write("Election Results\n")
+
+    # Write the second row
+    fw.write(' \n')
+
+    # Write the third row
+    fw.write('-----------------------------\n')
+
+    # Write the fourth row
+    fw.write(' \n')
+
+    # Write the fifth row
+    fw.write("Total Votes: " + str(totalVotes)+'\n')
+
+    # Write the sixth row
+    fw.write(' ')
+
+    # Write the seventh row
+    fw.write('-----------------------------\n')
+
+    # Write the eight row
+    fw.write(' \n')
+
+    # Write the ninth row
+    fw.write("Khan: " + str(candVotesPercCnt['Khan'][0]) + "% " + "(" + str(candVotesPercCnt['Khan'][1]) + ")"+'\n')
+
+    # Write the tenth row
+    fw.write("Correy: " + str(candVotesPercCnt['Correy'][0]) + "% " + "(" + str(candVotesPercCnt['Correy'][1]) + ")"+'\n')
+
+    # Write the eleventh row
+    fw.write("Li: " + str(candVotesPercCnt['Li'][0]) + "% " + "(" + str(candVotesPercCnt['Li'][1]) + ")"+'\n')
+
+    # Write the twelveth row
+    fw.write("O'Tooley: " + str(candVotesPercCnt["O'Tooley"][0]) + "% " + "(" + str(candVotesPercCnt["O'Tooley"][1]) + ")"+'\n')
+
+    # Write the thirteenth row
+    fw.write(' \n')
+
+    # Write the fourteenth row
+    fw.write('-----------------------------\n')
+
+    # Write the fifteenth row
+    fw.write(' \n')
+
+    # Write the sixteenth row
+    fw.write("Winner: " + winner[0]+'\n')
+
+    # Write the seventeenth row
+    fw.write(' \n')
+    
+    # Write the eighteenth row
+    fw.write('-----------------------------\n')
